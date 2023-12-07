62a8c0fef72b0d29598baf7b954489cc8050d84b
Kieronr142 <u09kr18@abdn.ac.uk>
2023-12-07 16:09:33 +0000

    GUI revision

--- sensstay.py
+++ sensstay.py
@@ -1,30 +1,63 @@
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 def backup_game_config():
-    # Ask the user for the game's name
-    game_name = input("Enter the game's name: ")
+    # Create a new Tkinter window
+    window = tk.Tk()
+    
+    # Name the window
+    window.title('SensStay')
 
-    # Ask the user for the config file path
-    config_path = input("Enter the full path to the game's config file: ")
+    # Set the window size and position
 
-    # Remove double quotes from the config_path
-    config_path = config_path.replace('"', '')
+    # find the center point
+    center_x = int(window.winfo_screenwidth() / 2 - 300)
+    center_y = int(window.winfo_screenheight() / 2 - 200)
+    window.geometry(f'600x400+{center_x}+{center_y}')
 
-    # Check if the config file exists
-    if not os.path.isfile(config_path):
-        print("The provided config file path does not exist.")
-        return
+    # Create a StringVar for the game's name
+    game_name_var = tk.StringVar()
 
-    # Create the backup directory
-    backup_dir = os.path.join(os.getcwd(), "Configs", game_name)
-    os.makedirs(backup_dir, exist_ok=True)
+    # Create an entry field for the game's name
+    game_name_entry = tk.Entry(window, textvariable=game_name_var)
+    game_name_entry.pack()
 
-    # Copy the config file to the backup directory
-    backup_path = os.path.join(backup_dir, os.path.basename(config_path))
-    shutil.copy2(config_path, backup_path)
+    # Create a button that lets the user select the config file
+    def select_config_file():
+        config_path = filedialog.askopenfilename()
+        config_path_var.set(config_path)
 
-    print(f"Config file for {game_name} has been backed up to {backup_path}")
+    config_path_var = tk.StringVar()
+    config_path_button = tk.Button(window, text="Select Config File", command=select_config_file)
+    config_path_button.pack()
+
+    # Create a button that backs up the config file when clicked
+    def backup_config():
+        game_name = game_name_var.get()
+        config_path = config_path_var.get()
+
+        # Check if the config file exists
+        if not os.path.isfile(config_path):
+            print("The provided config file path does not exist.")
+            return
+
+        # Create the backup directory
+        backup_dir = os.path.join(os.getcwd(), "Configs", game_name)
+        os.makedirs(backup_dir, exist_ok=True)
+
+        # Copy the config file to the backup directory
+        backup_path = os.path.join(backup_dir, os.path.basename(config_path))
+        shutil.copy2(config_path, backup_path)
+
+        print(f"Config file for {game_name} has been backed up to {backup_path}")
+
+    backup_button = tk.Button(window, text="Backup Config", command=backup_config)
+    backup_button.pack()
+
+    # Start the Tkinter event loop
+    window.mainloop()
 
 # Call the function
 backup_game_config()
