00b4547047021cb3116890929f7dd2c5ddae2c97
Kieronr142 <u09kr18@abdn.ac.uk>
2023-12-08 13:43:36 +0000

    Displays output

--- sensstay.py
+++ sensstay.py
@@ -62,18 +62,14 @@ def backup_game_config():
                                    command=select_config_file)
     config_path_button.pack(padx=10, pady=10)
 
-    # Create a label to display the selected configuration path
-    config_path_title_label = tk.Label(back_frame, text="Configuration Selected:")
-    config_path_title_label.pack()
-
-    # Create a label to display the selected configuration path
+    # Create a text box to display the selected configuration path
     # Limited to 30 characters long and wrap stops the output being displayed vertically
     config_path_text = tk.Text(back_frame, 
                                height=1, 
                                width=30, 
-                               wrap="none", 
-                               state='disabled',
-                               bg='#f0f0f0')
+                               wrap="none",
+                               bg='#f0f0f0',
+                               state='disabled')
     config_path_text.pack()
 
     # Create a horizontal scrollbar and attach it to the text box
@@ -121,21 +117,46 @@ def backup_game_config():
 
         # Check if the config file exists
         if not os.path.isfile(config_path):
-            print("The provided config file path does not exist.")
+            backup_path_text.config(state='normal') # Enable box editing
+            backup_path_text.delete('1.0', tk.END)  # Clear the text box
+            backup_path_text.insert(tk.END, 'Configuration file path does not exist')
+            backup_path_text.config(state='disabled') # Disable box editing
             return
+        
+        # Check if the game name exists
+        if len(game_name) == 0:
+            backup_path_text.config(state='normal') # Enable box editing
+            backup_path_text.delete('1.0', tk.END)  # Clear the text box
+            backup_path_text.insert(tk.END, 'Enter a valid game name')
+            backup_path_text.config(state='disabled') # Disable box editing
+        else:
+            # Create the backup directory
+            backup_dir = os.path.join(os.getcwd(), "Configs", game_name)
+            os.makedirs(backup_dir, exist_ok=True)
 
-        # Create the backup directory
-        backup_dir = os.path.join(os.getcwd(), "Configs", game_name)
-        os.makedirs(backup_dir, exist_ok=True)
+            # Copy the config file to the backup directory
+            backup_path = os.path.join(backup_dir, os.path.basename(config_path))
+            shutil.copy2(config_path, backup_path)
 
-        # Copy the config file to the backup directory
-        backup_path = os.path.join(backup_dir, os.path.basename(config_path))
-        shutil.copy2(config_path, backup_path)
 
-        print(f"Config file for {game_name} has been backed up to {backup_path}")
+            # Update the backup text box with the selected path
+            backup_path_text.config(state='normal') # Enable box editing
+            backup_path_text.delete('1.0', tk.END)  # Clear the text box
+            backup_path_text.insert(tk.END, 
+                                    f'Configuration file for {game_name} has been backed up to {backup_path}')
+            backup_path_text.config(state='disabled') # Disable box editing
 
     backup_button = tk.Button(back_frame, text="Backup Config", command=backup_config)
     backup_button.pack(padx=10, pady= 10)
+
+    # Create a text box to display the selected configuration path
+    # Limited to 30 characters long
+    backup_path_text = tk.Text(back_frame, 
+                               height=5, 
+                               width=30,
+                               bg='#f0f0f0',
+                               state='disabled')
+    backup_path_text.pack()
 
     # Start the Tkinter event loop
     window.mainloop()
