e7b508bacbd616035f03630fa7c5bcbf05db5ef4
Kieronr142 <u09kr18@abdn.ac.uk>
2023-12-08 10:10:17 +0000

    Updated GUI, added gitignore

--- sensstay.py
+++ sensstay.py
@@ -11,18 +11,22 @@ def backup_game_config():
     window.title('SensStay')
 
     # Set the window size and position
-
     # find the center point
     center_x = int(window.winfo_screenwidth() / 2 - 300)
     center_y = int(window.winfo_screenheight() / 2 - 200)
     window.geometry(f'600x400+{center_x}+{center_y}')
 
-    # Create a StringVar for the game's name
-    game_name_var = tk.StringVar()
+    # create a label for the backup frame
+    back_frame_label = tk.Label(window, text="Game Backup")
+    
+    # pack the label
+    back_frame_label.pack()
 
-    # Create an entry field for the game's name
-    game_name_entry = tk.Entry(window, textvariable=game_name_var)
-    game_name_entry.pack()
+    # create a frame for the config backup
+    back_frame = tk.Frame(window, bd=1, relief='solid')
+
+    # pack the frame into the window
+    back_frame.pack(ipadx= 10, ipady = 10)
 
     # Create a button that lets the user select the config file
     def select_config_file():
@@ -30,8 +34,21 @@ def backup_game_config():
         config_path_var.set(config_path)
 
     config_path_var = tk.StringVar()
-    config_path_button = tk.Button(window, text="Select Config File", command=select_config_file)
-    config_path_button.pack()
+    config_path_button = tk.Button(back_frame, 
+                                   text="Select Config File", 
+                                   command=select_config_file)
+    config_path_button.pack(padx=10, pady=10)
+
+    # Create a StringVar for the game's name
+    game_name_var = tk.StringVar()
+
+    # Create a label for the game's name entry field
+    game_name_label = tk.Label(back_frame, text="Game Name:")
+    game_name_label.pack()
+
+    # Create an entry field for the game's name
+    game_name_entry = tk.Entry(back_frame, textvariable=game_name_var)
+    game_name_entry.pack()
 
     # Create a button that backs up the config file when clicked
     def backup_config():
@@ -53,8 +70,8 @@ def backup_game_config():
 
         print(f"Config file for {game_name} has been backed up to {backup_path}")
 
-    backup_button = tk.Button(window, text="Backup Config", command=backup_config)
-    backup_button.pack()
+    backup_button = tk.Button(back_frame, text="Backup Config", command=backup_config)
+    backup_button.pack(padx=10, pady= 10)
 
     # Start the Tkinter event loop
     window.mainloop()
